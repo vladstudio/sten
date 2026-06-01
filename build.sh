2ca0d0ada4d4191a054a06b197aaca32c606058e
Vlad Gerasimov <vlad@vlad.studio>
2026-06-01 15:44:16 +0400

    Rename scripts/ to app-scripts/

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-source ../scripts/build-kit.sh
+source ../app-scripts/build-kit.sh
 build_app "Sten" \
   --info app/Sten/Info.plist \
   --resources "icons/*.png" \
